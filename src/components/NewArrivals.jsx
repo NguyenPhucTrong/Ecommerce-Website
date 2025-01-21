879e117077d82db123f1d7636a7ea2ec3f49afbc
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2025-01-21 23:05:31 +0700

    create and writing code item page (1)

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Autoplay } from 'swiper/modules';
+import Title from './Title';
+import Item from './Item';
+import { products } from '../assets/data';
 
 const NewArrivals = () => {
+    const [PopularProducts, setPopularProducts] = useState([])
+    useEffect(() => {
+        const data = products.slice(0, 7)
+        setPopularProducts(data)
+    }, [products])
     return (
-        <div>
-            NewArrivals
-        </div>
+        <section>
+            <Title title1={"New"} title2={"Arrivals"} titleStyles={"pd-10"} paraStyles={"!block"} />
+
+            {/* CONTAINER */}
+            <Swiper
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                breakpoints={{
+                    300: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    666: {
+                        slidesPerView: 3,
+                        spaceBetween: 30,
+                    },
+                    900: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                    1300: {
+                        slidesPerView: 5,
+                        spaceBetween: 30,
+                    },
+                }}
+
+                modules={[Autoplay]}
+                className="h-[399px] mt-5"
+            >{
+                    PopularProducts.map((product) => {
+                        return (
+                            <SwiperSlide key={product._id}>
+                                <Item product={product} />
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
+        </section>
     )
 }
 
