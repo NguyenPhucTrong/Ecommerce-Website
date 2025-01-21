958f6d45fd36b3b1d89ec0e7d232d294d305b18d
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2025-01-21 20:55:17 +0700

    features page edit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,22 +14,20 @@ const Features = () => {
                         <h4 className='medium-15'>MONEY-BACK GUARANTEE</h4>
                         <p>100% refund guaranteed if you're not satisfied</p>
                     </div>
-                </div>
-            </div> <div className='max-padd-container flexCenter flex-wrap gap-8 rounded-2xl'>
-                <div className='flexCenter gap-x-3'>
-                    <TbTruckDelivery className='text-3xl' />
-                    <div>
-                        <h4 className='medium-15'>MONEY-BACK GUARANTEE</h4>
-                        <p>100% refund guaranteed if you're not satisfied</p>
+                    <div className='flexCenter gap-x-3'>
+                        <TbTruckDelivery className='text-3xl' />
+                        <div>
+                            <h4 className='medium-15'>FREE SHIPPING & RETURNS </h4>
+                            <p>Free shipping available on all orders above $99</p>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className='max-padd-container flexCenter flex-wrap gap-8 rounded-2xl'>
-                <div className='flexCenter gap-x-3'>
-                    <BiSupport className='text-3xl' />
-                    <div>
-                        <h4 className='medium-15'>MONEY-BACK GUARANTEE</h4>
-                        <p>100% refund guaranteed if you're not satisfied</p>
+
+                    <div className='flexCenter gap-x-3'>
+                        <BiSupport className='text-3xl' />
+                        <div>
+                            <h4 className='medium-15'>24/7 ONLINE SUPPORT</h4>
+                            <p>Our team is here to assist you round the clock</p>
+                        </div>
                     </div>
                 </div>
             </div>
