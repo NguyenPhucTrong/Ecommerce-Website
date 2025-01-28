3030d3c69bfe5fd7ba4536de6e67896cba26b258
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2025-01-28 22:43:03 +0700

    making collection (3)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ShopContext } from '../context/shopContext'
+import { FaSearch } from 'react-icons/fa'
+import { useContext } from 'react'
 
 const Search = () => {
+    const { search, setSearch } = useContext(ShopContext)
+
     return (
-        <div>
-            Search
+        <div className='py-4 pb-7'>
+            <div className='text-center'>
+                <div className='inline-flex items-center justify-center px-3 py-1.5 rounded-full bg-white overflow-hidden w-full'>
+                    <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Search here...'
+                        className='border-none outline-none w-full bg-white text-sm' />
+                    <div>
+                        <FaSearch className="cursor-pointer" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,14 +1,15 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Search from '../components/Search'
 import { ShopContext } from '../context/shopContext';
+import Item from '../components/Item';
 
 const Collection = () => {
 
-    const { products } = useContext(ShopContext)
+    const { products, search } = useContext(ShopContext)
     const [category, setCategory] = useState([])
     const [sortType, setSortType] = useState('relevant')
-    const [filteredProducts, setFilteredProducts] = useState(second)
-    const [currentPages, setCurrentPages] = useState(1)
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
     const itemsPerPage = 10
 
     const toggleFilter = (value, setState) => {
@@ -47,8 +48,17 @@ const Collection = () => {
         let filtered = applyFilter()
         let sorted = applySorting(filtered)
         setFilteredProducts(sorted)
-        setCurrentPages(1)
-    }, [category, sortType, search, sortType])
+        setCurrentPage(1)
+    }, [category, sortType, sortType, search])
+
+    const getPaginatedProducts = () => {
+        const startIndex = (currentPage - 1) * itemsPerPage
+        const endIndex = startIndex + itemsPerPage
+        return filteredProducts.slice(startIndex, endIndex)
+    }
+
+    const totalPages = Math.ceil(filteredProducts.length /
+        itemsPerPage)
 
     return (
         <div className='max-padd-container !px-0'>
@@ -71,7 +81,7 @@ const Collection = () => {
                                     .map((cat) => (
                                         <label key={cat} className='flex gap-2 medium-14 text-gray-30'>
                                             <input
-
+                                                onChange={(e) => toggleFilter(e.target.value, setCategory)}
                                                 type='checkbox' value={cat} className='w-3' />
                                             {cat}
                                         </label>
@@ -79,11 +89,12 @@ const Collection = () => {
                             }
                         </div>
                     </div>
-                    <div>
-                        <h5>
+                    <div className='px-4 px-3 mt-6 bg-white rounded-xl'>
+                        <h5 className='h5 mb-4 '>
                             Sort by
                         </h5>
-                        <select name="" id="">
+                        <select onChange={(e) => setSortType(e.target.value)}
+                            className='border border-slate-900/5 outline-none text-gray-30 medium-14 h-8 w-full rounded px-2'>
                             <option value="relevant">Relevant</option>
                             <option value="low">Low</option>
                             <option value="high">High</option>
@@ -91,9 +102,22 @@ const Collection = () => {
                     </div>
                 </div>
                 {/* RIGHT SIDE */}
-                <div>
-                    <div>
-                        products
+                <div className='pr-5 rounded-l-xl'>
+                    <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4 gap-y-6'>
+                        {getPaginatedProducts().length > 0 ? (
+                            getPaginatedProducts().map((product) => (
+                                <Item key={product.id} product={product} />
+                            ))
+                        ) : (
+                            <p>
+                                No products found for the selected filters
+                            </p>
+                        )
+                        }
+                    </div>
+                    {/* PAGINATION */}
+                    <div className='flexCenter flex-wrap gap-4 mt-14 mb-10'>
+                        <p>button</p>
                     </div>
                 </div>
             </div>
